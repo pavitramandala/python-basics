bab25b50d05f8d7d9727962d9d452615c9147072
Pavitra Mandala <paviists23@gmail.com>
2026-07-24 12:55:21 +0530

    Add program to reverse a number in Python

---
+++ reverse_number.py
@@ -0,0 +1,14 @@
+# Program: Reverse Number
+# Language: Python
+
+num = int(input("Enter a number: "))
+
+temp = num
+rev = 0
+
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp = temp // 10
+
+print("Reverse Number =", rev)
